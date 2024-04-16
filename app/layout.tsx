51355f68be2897058b3319e04f100445cb896e06
hungpm372 <hungpm372@gmail.com>
2024-04-17 02:53:11 +0700

    Join meeting room

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,18 @@
+import { Toaster } from '@/components/ui/toaster'
+import { ClerkProvider } from '@clerk/nextjs'
+import '@stream-io/video-react-sdk/dist/css/styles.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
-import { ClerkProvider } from '@clerk/nextjs'
-import { Toaster } from '@/components/ui/toaster'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Yoom',
-  description: 'Yoom meeting'
+  description: 'Yoom meeting',
+  icons: {
+    icon: '/icons/logo.svg'
+  }
 }
 
 export default function RootLayout({
