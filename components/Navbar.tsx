72ececfceb37fb4af3f67fc14da8b5115e3e44b3
hungpm372 <hungpm372@gmail.com>
2024-04-16 20:05:06 +0700

    Add clerk authentication

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import MobileNav from './MobileNav'
+import { SignedIn, UserButton } from '@clerk/nextjs'
 
 const Navbar = () => {
   return (
@@ -11,6 +12,9 @@ const Navbar = () => {
         <p className='text-[26px] font-extrabold text-white max-sm:hidden'>Yoom</p>
       </Link>
       <div className='flex-between gap-5'>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
         <MobileNav />
       </div>
     </nav>
