bf284d62bd81a9ee1670684605e9888e88d2cea9
hungpm372 <hungpm372@gmail.com>
2024-04-17 12:43:49 +0700

    Join meeting with link

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -1,13 +1,14 @@
 'use client'
-import React, { useState } from 'react'
-import HomeCard from './HomeCard'
-import { useRouter } from 'next/navigation'
-import MeetingModal from './MeetingModal'
 import { useUser } from '@clerk/nextjs'
 import { Call, useStreamVideoClient } from '@stream-io/video-react-sdk'
-import { useToast } from './ui/use-toast'
-import { Textarea } from './ui/textarea'
+import { useRouter } from 'next/navigation'
+import { useState } from 'react'
 import ReactDatePicker from 'react-datepicker'
+import HomeCard from './HomeCard'
+import MeetingModal from './MeetingModal'
+import { Input } from './ui/input'
+import { Textarea } from './ui/textarea'
+import { useToast } from './ui/use-toast'
 
 const MeetingTypeList = () => {
   const router = useRouter()
@@ -153,6 +154,21 @@ const MeetingTypeList = () => {
         buttonText='Start Meeting'
         handleClick={createMeeting}
       />
+
+      <MeetingModal
+        isOpen={meetingState === 'isJoiningMeeting'}
+        onClose={() => setMeetingState(undefined)}
+        title='Type the link here'
+        className='text-center'
+        buttonText='Join Meeting'
+        handleClick={() => router.push(values.link)}
+      >
+        <Input
+          placeholder='Meeting link'
+          onChange={(e) => setValues({ ...values, link: e.target.value })}
+          className='border-none bg-dark-3 focus-visible:ring-0 focus-visible:ring-offset-0'
+        />
+      </MeetingModal>
     </section>
   )
 }
